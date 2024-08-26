fce0ebdb69156b54a34e0cc77e3aab733e17284f
ryzhov <9920529@gmail.com>
2024-08-26 15:28:23 +0300

    fix(SG-314): fix migrations to use schema

--- database/migrations/20240314124330-translations_table.js
+++ database/migrations/20240314124330-translations_table.js
@@ -2,54 +2,63 @@
 
 const Sequelize = require("sequelize");
 
-/** @type {import('sequelize-cli').Migration} */
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
 	async up(queryInterface) {
-		await queryInterface.createTable("translations", {
-			id: {
-				type: Sequelize.INTEGER,
-				primaryKey: true,
-				autoIncrement: true,
+		await queryInterface.createTable(
+			{
+				schema: process.env.DB_SCHEMA,
+				tableName: "translations",
 			},
-			region: {
-				type: Sequelize.STRING(10),
-				allowNull: false,
-			},
-			org_id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-			},
-			client_id: {
-				type: Sequelize.INTEGER,
-				references: {
-					model: "clients",
-					key: "id",
+			{
+				id: {
+					type: Sequelize.INTEGER,
+					primaryKey: true,
+					autoIncrement: true,
 				},
-				onDelete: "RESTRICT",
-			},
-			token_id: {
-				type: Sequelize.INTEGER,
-				references: {
-					model: "tokens",
-					key: "id",
+				region: {
+					type: Sequelize.STRING(10),
+					allowNull: false,
 				},
-				onDelete: "RESTRICT",
-			},
-			locale: {
-				type: Sequelize.STRING(15),
-				allowNull: false,
-			},
-			text: {
-				type: Sequelize.TEXT,
-				allowNull: false,
-			},
-			created_at: Sequelize.DATE,
-			updated_at: Sequelize.DATE,
-			deleted_at: Sequelize.DATE,
-		});
+				org_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+				},
+				client_id: {
+					type: Sequelize.INTEGER,
+					references: {
+						model: "clients",
+						key: "id",
+					},
+					onDelete: "RESTRICT",
+				},
+				token_id: {
+					type: Sequelize.INTEGER,
+					references: {
+						model: "tokens",
+						key: "id",
+					},
+					onDelete: "RESTRICT",
+				},
+				locale: {
+					type: Sequelize.STRING(15),
+					allowNull: false,
+				},
+				text: {
+					type: Sequelize.TEXT,
+					allowNull: false,
+				},
+				created_at: Sequelize.DATE,
+				updated_at: Sequelize.DATE,
+				deleted_at: Sequelize.DATE,
+			}
+		);
 	},
 
 	async down(queryInterface) {
-		await queryInterface.dropTable("translations");
+		await queryInterface.dropTable({
+			schema: process.env.DB_SCHEMA,
+			tableName: "translations",
+		});
 	},
 };
--- database/migrations/20240314124854-jobs_table.js
+++ database/migrations/20240314124854-jobs_table.js
@@ -2,53 +2,62 @@
 
 const Sequelize = require("sequelize");
 
-/** @type {import('sequelize-cli').Migration} */
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
 	async up(queryInterface) {
-		await queryInterface.createTable("jobs", {
-			id: {
-				type: Sequelize.INTEGER,
-				primaryKey: true,
-				autoIncrement: true,
+		await queryInterface.createTable(
+			{
+				schema: process.env.DB_SCHEMA,
+				tableName: "jobs",
 			},
-			region: {
-				type: Sequelize.STRING(10),
-				allowNull: false,
-			},
-			org_id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-			},
-			client_id: {
-				type: Sequelize.INTEGER,
-				references: {
-					model: "clients",
-					key: "id",
+			{
+				id: {
+					type: Sequelize.INTEGER,
+					primaryKey: true,
+					autoIncrement: true,
 				},
-				onDelete: "RESTRICT",
-			},
-			translator: {
-				type: Sequelize.STRING(20),
-				allowNull: false,
-			},
-			status: {
-				type: Sequelize.STRING(15),
-				allowNull: false,
-			},
-			locales: {
-				type: Sequelize.ARRAY(Sequelize.STRING(15)),
-				allowNull: false,
-			},
-			number_tokens: Sequelize.INTEGER,
-			number_translations: Sequelize.INTEGER,
-			launch_type: Sequelize.STRING(10),
-			created_at: Sequelize.DATE,
-			updated_at: Sequelize.DATE,
-			deleted_at: Sequelize.DATE,
-		});
+				region: {
+					type: Sequelize.STRING(10),
+					allowNull: false,
+				},
+				org_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+				},
+				client_id: {
+					type: Sequelize.INTEGER,
+					references: {
+						model: "clients",
+						key: "id",
+					},
+					onDelete: "RESTRICT",
+				},
+				translator: {
+					type: Sequelize.STRING(20),
+					allowNull: false,
+				},
+				status: {
+					type: Sequelize.STRING(15),
+					allowNull: false,
+				},
+				locales: {
+					type: Sequelize.ARRAY(Sequelize.STRING(15)),
+					allowNull: false,
+				},
+				number_tokens: Sequelize.INTEGER,
+				number_translations: Sequelize.INTEGER,
+				launch_type: Sequelize.STRING(10),
+				created_at: Sequelize.DATE,
+				updated_at: Sequelize.DATE,
+				deleted_at: Sequelize.DATE,
+			}
+		);
 	},
 
 	async down(queryInterface) {
-		await queryInterface.dropTable("jobs");
+		await queryInterface.dropTable({
+			schema: process.env.DB_SCHEMA,
+			tableName: "jobs",
+		});
 	},
 };
